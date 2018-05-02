72bf1f0ae99720321d26992f20778fb2571a7193
Rhiannon98 <mortensenrhi@gmail.com>
2018-05-02 12:47:33 -0700

    tmr move from r to m

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('dotenv').congfig();
 // Application dependencies
 const fs = require('fs');
 const express = require('express');
@@ -12,7 +13,7 @@ const bodyparser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
-const api_key = 'AIzaSyCoXYAtJ8tWx1VDuinGJgoUb0bO5KIPz-A';
+const api_key = token;
 
 
 // Database Setup
@@ -32,13 +33,13 @@ app.get('api/v1/map_test/:location', (req, res) => {
   console.log('in app.get');
 
   superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${userLocation}&key=${api_key}`)
-  .then(data => {
-    console.log(data.body);
-    res.send('success', res.body)
-  })
-  .catch(err => {
-    console.error('we have an error: ', err);
-  })
+    .then(data => {
+      console.log(data.body);
+      res.send('success', res.body)
+    })
+    .catch(err => {
+      console.error('we have an error: ', err);
+    })
 })
 
 app.get('/api/v1/parks/find', (req, res) => {
@@ -70,35 +71,22 @@ app.get('/api/v1/parks/find', (req, res) => {
       console.log(req.query.index);
 
 
-      //We have to select some random animals here. Should we parse out the data here on the server side? Or send the big thing over to the client model and do it there? Does it matter? We'll do server side for now...
-
-      // console.log(response.body.results[0]);
-
       //In case we want to allow user to choose number of animals displayed, we can modify the next line of code to make dynamic.
       let numAnimalsDisplayed = 5;
 
-      //Can we just create a new object property in the response.body like this?
+      //Can we just create a new object property in the response.body like this
       response.body.animals = [];
 
-      //Loop through results to select 5 sightings and choose the properties we want from the API data.
       for (var i = 0; i < numAnimalsDisplayed; i++) {
 
-        //Generate random index
         let randInd = Math.floor(Math.random() * response.body.results.length);
 
-
-        // console.log(response.body.results[randInd].taxon.preferred_common_name);
-
-        //TODO: Specify what we're grabbing from the API here. Maybe include some logic to elimate possibility of repeats. I think the chance of repeats is so small we might be able to ignore it.
-
         let currObj = {
-
           park: req.query.index,
           name: response.body.results[randInd].taxon.preferred_common_name !== null ? response.body.results[randInd].taxon.preferred_common_name : '',
           observed_on: response.body.results[randInd].observed_on !== null ? response.body.results[randInd].observed_on : '',
           wiki: response.body.results[randInd].taxon.wikipedia_url !== null ? response.body.results[randInd].taxon.wikipedia_url : '',
           image: response.body.results[randInd].taxon.default_photo.square_url !== null ? response.body.results[randInd].taxon.default_photo.square_url : '',
-
         };
 
         response.body.animals.push(currObj);
@@ -108,8 +96,6 @@ app.get('/api/v1/parks/find', (req, res) => {
       return response.body.animals;
 
     })
-
-    // .then(response => console.log(response.body.results[1].species_guess)
 
     .then(data => {
       console.log(data);
@@ -121,12 +107,29 @@ app.get('/api/v1/parks/find', (req, res) => {
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
+app.post('/usersub', (req, res) => {
+  client.query(
+    `INSERT INTO usertable(username, password) VALUES($1, $2)
+    ON CONFLICT DO NOTHING,`
+    [request.body.username, request.body.password],
+  )
+    .catch(console.error)
+    .then(queryTwo());
 
+  function queryTwo() {
+    client.query('INSERT INTO highscores(username, animals_spotted) VALUES($1, $2) ONCONFLICT DO NOTHING'
+    [request.body.username, 0])
+  }
+});
+
+app.put('/scorechange', (req, res) => {
+  client.query('UPDATE highscores SET animals_spotted=$1 WHERE username=$2;'
+  [/* this is the length of the array with animals checked by the user */ request.body.username])
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-//////////
-//TODO: Below here, we should do our psql database setup. Grab some sample code from previous labs that create tables if they don't exist and setup column names. This will be for user data. Something like primary key, username, password, number animals spotted, other stuff?
+////////// DB loaders \\\\\\\\\\\\\\
 
 function loadUsers() {
   // leaving what is in quotes blank
@@ -153,7 +156,7 @@ function loadScores() {
             highscores(user_id, username, animals_spotted)
             SELECT user_id, $1, $2
             FROM usertable
-            WHERE username=$1;
+            WHERE id=$1;
             `,
               [elem.user_id, elem.username, elem.animal_spotted]
             )
@@ -168,10 +171,9 @@ function loadDB() {
   client.query(`
     CREATE TABLE IF NOT EXISTS
     usertable (
-      user_id SERIAL PRIMARY KEY,
+      id SERIAL PRIMARY KEY,
       username VARCHAR(25) NOT NULL,
       password VARCHAR(25) NOT NULL,
-      email TEXT NOT NULL
     );
   `)
     .then(loadUsers)
@@ -182,10 +184,18 @@ function loadDB() {
   highscores (
     highscore_id SERIAL PRIMARY KEY,
     animals_spotted INTEGER,
-    user_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL REFERENCES usertable(id)
     );`
   )
     .then(loadScores)
     .catch(console.error);
+
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    locations(
+      user_id INTEGER NOT NULL REFERENCES usertable(id),
+      location TEXT NOT NULL
+    );`
+  )
 }
 

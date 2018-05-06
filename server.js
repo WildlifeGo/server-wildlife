94f947e39e5cd406f7d8da972b765e6252c7c362
Rhiannon Mortensen <34196086+Rhiannon98@users.noreply.github.com>
2018-05-06 13:34:03 -0700

    Update server.js
    zombie hoard killed.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Application dependencies
-//require('dotenv').congfig();
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
@@ -36,7 +35,6 @@ app.get('/api/v1/parks/googlemaps/:location', (req, res) => {
   }
   superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${api_key}`)
     .then(data => {
-      console.log(data.body.results[0].formatted_address, data.body.results[0].geometry.location);
       let locationData = [data.body.results[0].address_components[0].long_name, data.body.results[0].geometry.location];
       res.send(locationData);
     })
@@ -46,21 +44,15 @@ app.get('/api/v1/parks/googlemaps/:location', (req, res) => {
 });
 
 app.get('/api/v1/load_user', (req, res) => {
-  console.log('loading user');
   client.query(`SELECT * FROM usertable;`)
     .then(results => {
-      console.log(results.rows);
       let tableData = results.rows;
       res.send(tableData);
-      //res.send(tableData);
     });
 });
 
 app.get('/api/v1/parks/find', (req, res) => {
-  console.log('we hit the server');
-
-  console.log(req.query.lat);
-
+  
   let url = 'https://api.inaturalist.org/v1/observations';
 
   superagent.get(url)
@@ -87,24 +79,14 @@ app.get('/api/v1/parks/find', (req, res) => {
     })
     .then(response => {
 
-      console.log(response.body.results.length);
-
       response.body.results = response.body.results.filter(obj =>
         (obj.taxon !== null));
 
       response.body.results = response.body.results.filter(obj =>
         (obj.taxon.default_photo !== null));
 
-
-
-      console.log(response.body.results.length);
-
       response.body.results = response.body.results.filter(obj =>
         (obj.taxon.iconic_taxon_name !== null || '') && (obj.taxon.preferred_common_name !== null || '') && (obj.observed_on !== null || '') && (obj.taxon.wikipedia_url !== null || '') && (obj.taxon.default_photo.square_url !== null || ''));
-
-
-
-      console.log(response.body.results.length);
 
       var mammalsArray = response.body.results;
       var birdsArray = response.body.results;
@@ -114,57 +96,27 @@ app.get('/api/v1/parks/find', (req, res) => {
       var mammals = mammalsArray.filter(obj =>
         obj.taxon.iconic_taxon_name === 'Mammalia' || obj.taxon.iconic_taxon_name === 'Reptilia' || obj.taxon.iconic_taxon_name === 'Amphibia');
 
-      // console.log('Mammals: ', mammals[0].taxon.preferred_common_name);
-
-      console.log(mammals.length);
-
       var birds = birdsArray.filter(obj1 =>
         obj1.taxon.iconic_taxon_name === 'Aves');
-
-      // console.log('Birdies ', birds[0].taxon.preferred_common_name);
-
-      console.log(birds.length);
 
       var insects = insectsArray.filter(obj2 =>
         obj2.taxon.iconic_taxon_name === 'Insecta' || obj2.taxon.iconic_taxon_name === 'Arachnida');
 
-      console.log(insects.length);
-
-      // console.log('Buggers: ', insects[0].taxon.preferred_common_name);
-
       var plants = plantsArray.filter(obj3 =>
         obj3.taxon.iconic_taxon_name === 'Plantae');
 
-      console.log(plants.length);
-
-
-      // console.log('Plants: ', plants[0].taxon.preferred_common_name);
-
-      //In case we want to allow user to choose number of animals displayed, we can modify the next line of code to make dynamic.
       let numAnimalsDisplayed = 4;
 
-      //Can we just create a new object property in the response.body like this
       response.body.animals = [];
 
-
-      //Loop through results to select 5 sightings and choose the properties we want from the API data.
-
-
-
       for (var i = 0; i < numAnimalsDisplayed; i++) {
-
-        //Generate random index
         let animals = [mammals, birds, insects, plants];
         let randInd0 = Math.floor(Math.random() * mammals.length);
         let randInd1 = Math.floor(Math.random() * birds.length);
         let randInd2 = Math.floor(Math.random() * insects.length);
         let randInd3 = Math.floor(Math.random() * plants.length);
         let randInd = [randInd0, randInd1, randInd2, randInd3];
-
-        console.log(randInd);
-
-        console.log(animals[i].length);
-
+        
         if (animals[i].length === 0) {
           animals[i] = animals[1];
         }
@@ -191,7 +143,6 @@ app.get('/api/v1/parks/find', (req, res) => {
     })
 
     .then(data => {
-      console.log(data);
       res.send(data);
     })
     .catch(console.error);
@@ -202,7 +153,6 @@ app.post('/api/v1/signin', (req, res) => {
     username,
     password
   } = req.body;
-  console.log('app.post');
   client.query(
     'INSERT INTO usertable(username, password) VALUES($1, $2) ON CONFLICT DO NOTHING', [username, password]
   )
@@ -211,11 +161,7 @@ app.post('/api/v1/signin', (req, res) => {
 });
 
 app.put('/api/v1/parks/submit', (req, res) => {
-
-  console.log(req.body);
   let storedUser = req.body.userName;
-  console.log(storedUser);
-
 
   let uniqueSightings = [];
 
@@ -224,8 +170,6 @@ app.put('/api/v1/parks/submit', (req, res) => {
       if (!x.rows[0].animals) {
         x.rows[0].animals = [];
       }
-      console.log(x.rows[0].animals);
-      console.log(req.body.animals);
       let allSightings = req.body.animals.concat(x.rows[0].animals);
       uniqueSightings = allSightings.filter(function (item, pos) {
         return allSightings.indexOf(item) === pos;
@@ -234,10 +178,8 @@ app.put('/api/v1/parks/submit', (req, res) => {
 
     })
     .then(y => {
-      console.log(y);
       let arrayStr = JSON.stringify(y);
       let formattedArray = arrayStr.replace('[', '{').replace(']', '}');
-      console.log(formattedArray);
       client.query(
         `UPDATE usertable
         SET animals = '${formattedArray}' 
@@ -245,7 +187,6 @@ app.put('/api/v1/parks/submit', (req, res) => {
       ).then(
         client.query(`SELECT * FROM usertable WHERE username='${storedUser}';`)
           .then(results => {
-            console.log('final from server ' + results.rows[0].animals);
             res.send(results.rows[0].animals);
           })
       );
@@ -255,11 +196,6 @@ app.put('/api/v1/parks/submit', (req, res) => {
 
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
-
-// app.put('/scorechange', (req, res) => {
-//   client.query('UPDATE highscores SET animals_spotted=$1 WHERE username=$2;'
-//   [/* this is the length of the array with animals checked by the user */ request.body.username])
-// });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
@@ -277,9 +213,4 @@ function loadDB() {
       animals text[]
     );
   `);
-  // .then(
-  //   client.query(`
-  // INSERT INTO usertable(username, password, animals) VALUES('test','test','{test}') ON CONFLICT DO NOTHING;
-  // `)
-  // )
 }
